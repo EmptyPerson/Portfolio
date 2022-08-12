cb981bf1bb4a328668cf87dbab9d940fee44ee2d
Булахов Владислав <vladislav.bulahov@yandex.ru>
2022-08-13 02:35:51 +0300

    block job

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
 
 
 function App() {
@@ -7,32 +8,46 @@ function App() {
   return (
      <div>
          <Navbar/>
-         <div className="intro">
+         <Intro/>
+         <div className="works">
              <div className="container">
-                 <div className="intro_inner">
-                     <div className="intro_content">
-                         <h2 className="intro_subtitle">Hello I'm</h2>
-                         <h1 className="intro_title">Bulakhov Vladislav</h1>
-                         <div className="intro_text">Front Web Developer</div>
-                         <div className="social">
-                             <a className="social_link" href="https://t.me/seenager" target="_blank">
-                                 <img src="images/telegram.svg" alt="" className='git_favicon'/>
-                             </a>
-                             <a className="social_link" href="https://www.facebook.com/profile.php?id=100006243310002" target="_blank">
-                                 <img src="images/facebook.png" alt=""/>
-                             </a>
-                             <a className="social_link" href="https://instagram.com/bulakhov.v" target="_blank">
-                                 <img src="images/instagram.png" alt=""/>
-                             </a>
-                             <a className="social_link" href="https://github.com/EmptyPerson" target="_blank">
-                                    <img src="images/github.svg" alt="" className='git_favicon'/>
-                             </a>
+                 <h3 className='text_center'>My works</h3>
+                 <div className="portfolio">
+                     <div className="work">
+                         <div className="work_logo">
+                            <h2 className='work_name'>HOTEL</h2>
                          </div>
-
-                         <button className="btn" type="button">SEE MY RESUME</button>
+                         <div className="work_content">
+                             <div className="work_cat">category: website</div>
+                             <div className="work_title">
+                                 HOTEL
+                                <div className="work_data">2022</div>
+                             </div>
+                         </div>
                      </div>
-                     <div className='photo-container'>
-                            <img className="intro_photo" src="images/photo.png" alt=""/>
+                     <div className="work">
+                         <div className="work_logo">
+                             <h2 className='work_name'>QUIZ</h2>
+                         </div>
+                         <div className="work_content">
+                             <div className="work_cat">category: website</div>
+                             <div className="work_title">
+                                 Quiz
+                                 <div className="work_data">2022</div>
+                             </div>
+                         </div>
+                     </div>
+                     <div className="work">
+                         <div className="work_logo">
+                             <h2 className='work_name'>crypto<br/>charts</h2>
+                         </div>
+                         <div className="work_content">
+                             <div className="work_cat">category: website</div>
+                             <div className="work_title">
+                                 crypto charts
+                                 <div className="work_data">2021</div>
+                             </div>
+                         </div>
                      </div>
                  </div>
              </div>
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Intro.css'
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="container">
+                <div className="intro_inner">
+                    <div className="intro_content">
+                        <h2 className="intro_subtitle">Hello I'm</h2>
+                        <h1 className="intro_title">Bulakhov Vladislav</h1>
+                        <div className="intro_text">Frontend Web Developer</div>
+                        <div className="social">
+                            <a className="social_link" href="https://t.me/seenager" target="_blank">
+                                <img src="images/telegram.svg" alt="" className='git_favicon'/>
+                            </a>
+                            <a className="social_link" href="https://www.facebook.com/profile.php?id=100006243310002" target="_blank">
+                                <img src="images/facebook.png" alt=""/>
+                            </a>
+                            <a className="social_link" href="https://instagram.com/bulakhov.v" target="_blank">
+                                <img src="images/instagram.png" alt=""/>
+                            </a>
+                            <a className="social_link" href="https://github.com/EmptyPerson" target="_blank">
+                                <img src="images/github.svg" alt="" className='git_favicon'/>
+                            </a>
+                        </div>
+
+                        <button className="btn" type="button">SEE MY RESUME</button>
+                    </div>
+                    <div className='photo-container'>
+                        <img className="intro_photo" src="images/photo.png" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
